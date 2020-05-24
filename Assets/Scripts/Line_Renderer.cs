1b2dacf0d70ad9f53e9378ad71fb984cbd2df3d5
sgexclsv <sgbono37@gmail.com>
2020-05-24 18:41:34 +0900

    VR build
    1. '플러그인' 폴더에 있는 빌드할 때 충돌을 일으키는 파일 삭제
    2.  라인렌더러의 vertex추가 방식 변경
    3. 라인렌더러를 카메라에서 떼서 SDK의 자손 프리팹인 컨트롤러 프리팹에 붙여둠
    4. PC에서는 마우스 클릭으로 그릴 수 있고 VR기기에서는 컨트롤러 트리거를 당겨서 그릴 수 있음
    
    + 컨트롤러 프리팹의 자손 프리팹인 'dot'은 항상 컨트롤러가 가리키는 방향에서 일정거리 떨어진 채 떠 있음. 이 'dot'의 좌표를 가져와 그림을 그릴 때 사용함

---
+++ Assets/Scripts/Line_Renderer.cs
@@ -0,0 +1,122 @@
+﻿#if !UNITY_EDITOR && UNITY_ANDROID 
+#define ANDROID_DEVICE
+#endif
+
+using Pvr_UnitySDKAPI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line_Renderer : MonoBehaviour
+{
+
+    public LibPdInstance pdPatch;
+    List<Vector3> linePoints = new List<Vector3>();
+    public GameObject dot2;
+    public GameObject Camera2;
+
+    private GameObject currentLine;
+
+    private Vector3 mouseWorld;
+    private Vector3 tempFingerPos;
+
+    public float startWidth = 0.1f;
+    public float endWidth = 0.1f;
+    public float threshold = 0.001f;
+
+    Camera thisCamera;
+    LineRenderer lineRenderer;
+
+    Color lineColor_1_start = new Color(0,1,1,1);
+    Color lineColor_1_end = new Color(1, 1, 1, 1);
+    private int vertexCount = 0;
+ 
+
+    void Awake()
+    {
+       thisCamera = Camera2.GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        // 입력이 없을 때는 퓨어데이터모듈에 데이터0을 보냄
+        //if ((Input.GetMouseButton(0) == false && (Input.GetMouseButton(1) == false)))
+        /*
+        if (Pvr_UnitySDKAPI.Controller.UPvr_GetKey(0, Pvr_KeyCode.TRIGGER) == false)
+        {
+            
+        }
+        else
+        {
+            
+        }
+        */
+
+        //클릭 시작하면 Line을 만듬
+        if (Pvr_UnitySDKAPI.Controller.UPvr_GetKeyDown(0, Pvr_KeyCode.TRIGGER)|| Input.GetMouseButtonDown(0))
+        {
+            CreateLine();
+         }
+
+        //클릭한 채로 유지하고 있으면 Line을 연장함
+        
+            if (Pvr_UnitySDKAPI.Controller.UPvr_GetKey(0, Pvr_KeyCode.TRIGGER))
+            {
+                tempFingerPos = new Vector3(dot2.transform.position.x, dot2.transform.position.y, dot2.transform.position.z);
+                UpdateLine(tempFingerPos);
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                tempFingerPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, thisCamera.nearClipPlane * 100);
+                mouseWorld = thisCamera.ScreenToWorldPoint(tempFingerPos);
+                UpdateLine(mouseWorld);
+            }
+        else
+        {
+            //  pdPatch.SendFloat("Playing", 0);
+        }
+        // pdPatch.SendFloat("mouseY", (int)((tempFingerPos.y + 17) / 2.43));
+
+        //원래코드
+        //Vector3 tempFingerPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, thisCamera.nearClipPlane * 100);
+        //mouseWorld = thisCamera.ScreenToWorldPoint(tempFingerPos);
+        //if (Vector3.Distance(mouseWorld, fingerPositions[fingerPositions.Count - 1]) > .1f)
+        //{
+        //   UpdateLine(mouseWorld);
+        //}
+        // pdPatch.SendFloat("mouseY", (int)((mouseWorld.y + 17) / 2.43));
+    }
+
+    void CreateLine()
+    {
+        currentLine = new GameObject("line");
+   
+        lineRenderer = currentLine.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+
+        // 지정해놓은 public 변수에 맞춰서 선의 width값 지정해주는 거 추가함
+        lineRenderer.startWidth = startWidth;
+        lineRenderer.endWidth = endWidth;
+
+        // 선 색깔 지정
+        lineRenderer.startColor = lineColor_1_start;
+        lineRenderer.endColor = lineColor_1_end;
+
+        // Line의 vertex 개수를 세는 변수를 초기화
+        vertexCount = 0;
+
+        //처음 Line을 만들면 vertex가 기본적으로 두 개 생김. 이걸 없애야 함.
+        lineRenderer.positionCount = 0;
+    }
+
+    void UpdateLine(Vector3 newFingerPos)
+    {
+        // pdPatch.SendFloat("Playing", 1);
+        // pdPatch.SendFloat("mouseY", (int)((newFingerPos.y + 17) / 2.43));
+        lineRenderer.positionCount++;
+        lineRenderer.SetPosition(vertexCount, newFingerPos);
+        vertexCount++;
+  
+    }
+
+
+}
---
+++ Assets/Scripts/OSC_sender.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OSC_sender : MonoBehaviour
+{
+
+    public OSC osc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        OscMessage message0 = new OscMessage();
+        message0.address = "/toggle";
+        int toggle = 1;
+        message0.values.Add(toggle);
+        osc.Send(message0);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        OscMessage message = new OscMessage();
+        message.address = "/demo";
+        int demo = 1;
+        message.values.Add(demo);
+        osc.Send(message);
+
+    }
+}
