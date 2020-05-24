004bca635c417d42b94d3e668eca6dd15ef76cc2
sgexclsv <sgbono37@gmail.com>
2020-05-24 22:27:10 +0900

    minor Repair
    라인렌더러 스크립트에 브랜치간의 충돌사항이 겹쳐 작성되는 바람에 씬이 실행되지 않는 문제를 해결

--- Assets/Scripts/Line_Renderer.cs
+++ Assets/Scripts/Line_Renderer.cs
@@ -1,13 +1,11 @@
-<<<<<<< HEAD
-﻿using System.Collections.Generic;
-=======
+
 ﻿#if !UNITY_EDITOR && UNITY_ANDROID 
 #define ANDROID_DEVICE
 #endif
 
 using Pvr_UnitySDKAPI;
 using System.Collections.Generic;
->>>>>>> Develop
+
 using UnityEngine;
 
 public class Line_Renderer : MonoBehaviour
@@ -15,33 +13,7 @@ public class Line_Renderer : MonoBehaviour
 
     public LibPdInstance pdPatch;
     List<Vector3> linePoints = new List<Vector3>();
-<<<<<<< HEAD
 
-    private GameObject currentLine;
-    private Vector3 mouseWorld;
-
-    public float startWidth = 1.0f;
-    public float endWidth = 1.0f;
-    public float threshold = 0.001f;
-
-    public List<Vector3> fingerPositions;
-
-    Camera thisCamera;
-    LineRenderer lineRenderer;
-
-    Color left1 = new Color(0,1,1,1);
-    Color left2 = new Color(1, 1, 1, 0);
-    Color right1 = new Color(1,0,1,1);
-    Color right2 = new Color(1, 1, 1, 0);
-
-
-
-
-    void Awake()
-    {
-        thisCamera = Camera.main;
-        lineRenderer = GetComponent<LineRenderer>();
-=======
     public GameObject dot2;
     public GameObject Camera2;
 
@@ -65,106 +37,12 @@ public class Line_Renderer : MonoBehaviour
     void Awake()
     {
        thisCamera = Camera2.GetComponent<Camera>();
->>>>>>> Develop
+
     }
 
     void Update()
     {
-<<<<<<< HEAD
-        //no mouse input
-        if ((Input.GetMouseButton(0) == false && (Input.GetMouseButton(1) == false)))
-        {
-            pdPatch.SendFloat("Playing", 0);
-        }
-        else
-        {
-            pdPatch.SendFloat("Playing", 1);
-        }
 
-        //left click
-        if (Input.GetMouseButtonDown(0))
-        {
-
-            CreateLine(0);
-        }
-
-        if (Input.GetMouseButton(0))
-        {
-            Vector3 tempFingerPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, thisCamera.nearClipPlane * 100);
-            mouseWorld = thisCamera.ScreenToWorldPoint(tempFingerPos);
-
-
-            //if (Vector3.Distance(mouseWorld, fingerPositions[fingerPositions.Count - 1]) > .1f)
-            //{
-                UpdateLine(mouseWorld);
-            //}
-
-            pdPatch.SendFloat("mouseY", (int)((mouseWorld.y + 17) / 2.43));
-
-
-        }
-
-        //right click
-        if (Input.GetMouseButtonDown(1))
-        {
-
-            CreateLine(1);
-        }
-
-        if (Input.GetMouseButton(1))
-        {
-            Vector3 tempFingerPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, thisCamera.nearClipPlane * 100);
-            mouseWorld = thisCamera.ScreenToWorldPoint(tempFingerPos);
-
-
-           // if (Vector3.Distance(mouseWorld, fingerPositions[fingerPositions.Count - 1]) > .1f)
-            //{
-                UpdateLine(mouseWorld);
-            //}
-
-
-            pdPatch.SendFloat("mouseY", (int)((mouseWorld.y + 17) / 2.43));
-        }
-
-    }
-
-
-
-
-    void CreateLine(int mouseButton)
-    {
-        currentLine = new GameObject("line");
-
-        lineRenderer = currentLine.AddComponent<LineRenderer>();
-        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            lineRenderer.SetColors(left1,right1);
-        }
-        else if (Input.GetMouseButtonDown(1))
-        {
-            lineRenderer.SetColors(right1, left1);
-        }
-
-
-        fingerPositions.Clear();
-            Vector3 tempPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, thisCamera.nearClipPlane * 100);
-
-        fingerPositions.Add((thisCamera.ScreenToWorldPoint(tempPos)));
-        fingerPositions.Add((thisCamera.ScreenToWorldPoint(tempPos)));
-
-        lineRenderer.SetPosition(0, fingerPositions[0]);
-        lineRenderer.SetPosition(1, fingerPositions[1]);
-    }
-
-
-    void UpdateLine(Vector3 newFingerPos)
-    {
-        fingerPositions.Add(newFingerPos);
-        lineRenderer.positionCount++;
-        lineRenderer.SetPosition(lineRenderer.positionCount - 1, newFingerPos);
-=======
         // 입력이 없을 때는 퓨어데이터모듈에 데이터0을 보냄
         //if ((Input.GetMouseButton(0) == false && (Input.GetMouseButton(1) == false)))
         /*
@@ -243,7 +121,7 @@ public class Line_Renderer : MonoBehaviour
         lineRenderer.SetPosition(vertexCount, newFingerPos);
         vertexCount++;
   
->>>>>>> Develop
+
     }
 
 
