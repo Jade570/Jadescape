98a5976f792ddd97adc0443913f39c4597f6ef95
sgexclsv <sgbono37@gmail.com>
2020-06-07 00:00:47 +0900

    오로라 수정
    오로라 길이 무한
    오로라 생길때 0,0,0좌표에서 흰색 번쩍임이 있던 문제 해결

--- Assets/Scripts/Aurora_edited.cs
+++ Assets/Scripts/Aurora_edited.cs
@@ -64,21 +64,26 @@ public class Aurora_edited : MonoBehaviour {
 
         p_mEmission.enabled = false;
         p_mMain.startSpeed = 0;
-        p_mMain.maxParticles = auroraParticlesCount;
 
         pRenderer.material = auroraMaterialMain;
         pRenderer.renderMode = ParticleSystemRenderMode.VerticalBillboard;
         pRenderer.maxParticleSize = 100f;
 
-        SetParticleCount ();
+        p_mMain.maxParticles = 10;
+        p_Particles = new ParticleSystem.Particle[10];
+       // pSystem.Emit (100);
+        pSystem.GetParticles (p_Particles);
+
     }
 
     public void SetParticleCount () {
-
-        p_Particles = new ParticleSystem.Particle[auroraParticlesCount];
-        pSystem.Emit (auroraParticlesCount);
+        if(vertexs.Count>10)
+        {
+        p_mMain.maxParticles = vertexs.Count + 10;
+        p_Particles = new ParticleSystem.Particle[vertexs.Count];
+        pSystem.Emit (vertexs.Count);
         pSystem.GetParticles (p_Particles);
-
+        }
     }
 
     public void GradientSet (Color NewGrad) {
@@ -108,38 +113,41 @@ public class Aurora_edited : MonoBehaviour {
     }
 
     //Base Aurora Update
-    private void FixedUpdate () {
+    private void Update () {
 
-        for (int i = 0; i < vertexs.Count; i++) {
+        if (vertexs.Count > 1) {
+            for (int i = 0; i < vertexs.Count; i++) {
 
-            float time = i / (float) (vertexs.Count - 1);
+                float time = i / (float) (vertexs.Count - 1);
 
-            float perlin = 0;
+                float perlin = 0;
 
-            perlin = Mathf.PerlinNoise (Time.time * auroraAnimationFrequency, time * auroraCurvature);
-            float offset = perlin * 2f - 1f;
+                perlin = Mathf.PerlinNoise (Time.time * auroraAnimationFrequency, time * auroraCurvature);
+                float offset = perlin * 2f - 1f;
 
-            Vector3 p_Position;
-            // 써큘러가 아니면 포지션을 정하는 방식은 일단 다음과 같다.
-            //if (IsMakingDone == true)
-            p_Position = vertexs[i]; //+ transform.position;
-            //else p_Position = vertexs[i] + transform.position;
-            //p_Position = new Vector3 (i+offset * auroraSizes.x, i+0, i+auroraSizes.z) + transform.position;
-            // Quaternion.Euler(0, auroraRotation + angleOffset, 0) * new Vector3(offset * auroraSizes.x, 0, auroraSizes.z) + transform.position;
+                Vector3 p_Position;
+                // 써큘러가 아니면 포지션을 정하는 방식은 일단 다음과 같다.
+                //if (IsMakingDone == true)
+                p_Position = vertexs[i]; //+ transform.position;
+                //else p_Position = vertexs[i] + transform.position;
+                //p_Position = new Vector3 (i+offset * auroraSizes.x, i+0, i+auroraSizes.z) + transform.position;
+                // Quaternion.Euler(0, auroraRotation + angleOffset, 0) * new Vector3(offset * auroraSizes.x, 0, auroraSizes.z) + transform.position;
 
-            //evaluate 함수는 
-            Color p_Color = auroraColorMain.Evaluate (time);
+                //evaluate 함수는 
+                Color p_Color = auroraColorMain.Evaluate (time);
 
-            float sizeY = auroraSizes.y;
+                float sizeY = auroraSizes.y;
 
-            //if (vertexs.GetRange)
-            p_Particles[i].position = p_Position;
-            p_Particles[i].startSize3D = new Vector3 (auroraParticleThickness, sizeY, auroraParticleThickness);
-            p_Particles[i].startColor = p_Color;
+                //if (vertexs.GetRange)
+                p_Particles[i].position = p_Position;
+                p_Particles[i].startSize3D = new Vector3 (auroraParticleThickness, sizeY, auroraParticleThickness);
+                p_Particles[i].startColor = p_Color;
+
+            }
+            pSystem.SetParticles (p_Particles, vertexs.Count);
+            //오로라 오브젝트만 블러처리가 되도록 카메라에 컴포넌트를 달아두었음. 그러려면 레이어8에 지정되어야함
 
         }
-        pSystem.SetParticles (p_Particles, vertexs.Count);
-        //오로라 오브젝트만 블러처리가 되도록 카메라에 컴포넌트를 달아두었음. 그러려면 레이어8에 지정되어야함
-
     }
+
 }
